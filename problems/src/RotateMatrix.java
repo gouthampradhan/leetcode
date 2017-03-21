fa570a740ac7b468577376c6e2170d33caa832e3
gouthampradhan <goutham.vidya.pradhan@gmail.com>
2017-03-22 00:45:41 +0100

    Accepted

---
+++ problems/src/RotateMatrix.java
@@ -0,0 +1,43 @@
+/**
+ * Created by gouthamvidyapradhan on 21/03/2017.
+ * Accepted
+ */
+public class RotateMatrix
+{
+    /**
+     * Main method
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception
+    {
+        int[][] A = {{1,2,3}, {4,5,6}, {7,8,9}};
+        new RotateMatrix().rotate(A);
+        for (int i = 0; i < A.length; i ++)
+        {
+            for(int j = 0; j < A[0].length; j ++)
+            {
+                System.out.println(A[i][j]);
+            }
+        }
+    }
+
+    public void rotate(int[][] matrix)
+    {
+        int lc = 0, tr = 0, rc = matrix[0].length - 1, br = matrix.length - 1;
+        while(tr < br)
+        {
+            for(int i = lc, j = tr, k = rc, l = br; i < rc && j < br && k > lc && l > tr; i ++, j ++, k--, l--)
+            {
+                int temp1 = matrix[j][rc];
+                matrix[j][rc] = matrix[tr][i];
+                int temp2 = matrix[br][k];
+                matrix[br][k] = temp1;
+                temp1 = matrix[l][lc];
+                matrix[l][lc] = temp2;
+                matrix[tr][i] = temp1;
+            }
+            lc ++; tr ++; rc --; br --;
+        }
+    }
+}
