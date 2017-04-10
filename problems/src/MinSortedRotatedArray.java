ad6e539446b0eee88ad30c7d9781b97984dbbb24
gouthampradhan <goutham.vidya.pradhan@gmail.com>
2017-04-10 02:54:54 +0200

    Accepted

---
+++ problems/src/MinSortedRotatedArray.java
@@ -0,0 +1,36 @@
+/**
+ * Created by gouthamvidyapradhan on 10/04/2017.
+ * Accepted
+ */
+public class MinSortedRotatedArray
+{
+    /**
+     * Main method
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception
+    {
+        int[] A = {5, 1, 2, 3, 4};
+        System.out.println(new MinSortedRotatedArray().findMin(A));
+    }
+
+    public int findMin(int[] nums)
+    {
+        if(nums.length == 0) return 0;
+        else if(nums.length == 1) return nums[0];
+        int low = 0, high = nums.length - 1;
+        while(low < high)
+        {
+            int mid = (low + high) / 2;
+            if(mid > 0 && nums[mid] < nums[mid - 1])
+                return nums[mid];
+            if(nums[low] > nums[mid])
+                high = mid - 1;
+            else if(nums[high] < nums[mid])
+                low = mid + 1;
+            else high = mid - 1;
+        }
+        return nums[low];
+    }
+}
