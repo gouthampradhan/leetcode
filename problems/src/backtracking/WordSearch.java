0aa4d60a8719742a841bfe51356f0257a5386764
gouthampradhan <goutham.vidya.pradhan@gmail.com>
2017-05-04 21:36:45 +0200

    Code refactor

--- problems/src/backtracking/WordSearch.java
+++ problems/src/backtracking/WordSearch.java
@@ -1,6 +1,22 @@
+package backtracking;
+
 /**
  * Created by PRADHANG on 4/13/2017.
- * Accepted
+ Given a 2D board and a word, find if the word exists in the grid.
+
+ The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+ For example,
+ Given board =
+
+ [
+ ['A','B','C','E'],
+ ['S','F','C','S'],
+ ['A','D','E','E']
+ ]
+ word = "ABCCED", -> returns true,
+ word = "SEE", -> returns true,
+ word = "ABCB", -> returns false.
  */
 public class WordSearch
 {
